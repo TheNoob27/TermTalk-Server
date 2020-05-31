59500b3a0a9a79bc1f5b5f6355376125b82af92d
TorchedSammy <38820196+TorchedSammy@users.noreply.github.com>
2020-05-31 19:13:10 -0400

    login

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ const http = require('http').createServer()
 const io = require('socket.io')(http)
 const port = 3000
 
-io.on('connection', (socket) => {
-    console.log('connected')
-    socket.on('message', (data) => {
-        console.log(data)
-        socket.broadcast.emit('message', data)
+io.on("connection", (socket) => {
+    console.log('User connected.')
+    socket.on("login", (data) => {
+		console.log("LOGIN DATA\n%s", data)
+		socket.emit("ready")
+	})
+	socket.on('message', (data) => {
+        console.log("MESSAGE DATA\n%s", data)
+        socket.broadcast.emit("message", data)
+        socket.emit("ready")
     })
 })
 
-io.on('disconnect', (data) => {
-    console.log('disconnected')
+io.on("disconnect", (data) => {
+    console.log("User disconnected.")
 })
 
 http.listen(port, () => {
