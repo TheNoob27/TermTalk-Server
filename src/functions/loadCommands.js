5021e63e92a46a2d77399434e748a1c5c046706c
TheOnlyKirb <nicoloffaj@g.cofc.edu>
2020-06-12 12:11:00 -0400

    this should in theory, work

--- src/functions/loadCommands.js
+++ src/functions/loadCommands.js
@@ -1,15 +1,15 @@
-const cache = require("../../serverCache.js")
-const fs = require("fs")
 function load() {
-    fs.readdir('../commands', (err, files) => {
-    if (err) console.log(`Error while loading command.\nError BeloW\n----------------------------------\n${err}\n----------------------------------\n`)
-    files.forEach(f => {
-        console.log(`Loading command ${f.substr(f.length, f.length-3)}!`)
-        if (!f.endsWith(".js")) return;
-        const props = require(`../commands/${f}`)
-        cache.addons.hardCommands.set(props.data.name, props);
+    const cache = require("../../serverCache.js")
+    const fs = require("fs")
+    fs.readdir('src/commands', (err, files) => {
+        if (err) console.log(`Error while loading command.\nError Below\n----------------------------------\n${err}\n----------------------------------\n`)
+        files.forEach(f => {
+            console.log(`Loading command ${f}!`)
+            if (!f.endsWith(".js")) return;
+            const props = require(`../commands/${f}`)
+            cache.addons.hardCommands.set(props.data.name, props);
+        });
     });
-});
 }
 
 module.exports = load;
