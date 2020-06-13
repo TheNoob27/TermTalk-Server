715468c32d22e314fc2af19b700308693dc365a6
TheOnlyKirb <nicoloffaj@g.cofc.edu>
2020-06-12 23:39:03 -0400

    why did that get merged like that

--- src/functions/loadHistory.js
+++ src/functions/loadHistory.js
@@ -1,5 +1,5 @@
 function loadHistory(cache, io, socketID) {
-    for(let i = 0; i< cache.addons.monitor.chat.chatHistory.length; i++) {
+    for(let i = 0; i< cache.addons.chat.chatHistory.length; i++) {
         if(!cache.addons.chat.chatHistory[i]) break;
         io.sockets.connected[socketID].emit('msg', { username: "Server", tag: "0000", msg: `${cache.addons.chat.chatHistory[i]}`, uid: "Server", server: true })
     }
