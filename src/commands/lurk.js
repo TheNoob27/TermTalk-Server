22deacbaebf233c7eed719109ffbad7c275ff0af
Steven Webster <steven@is-just-a.dev>
2020-06-18 19:26:16 -0400

    Fixes

--- src/commands/lurk.js
+++ src/commands/lurk.js
@@ -2,26 +2,37 @@ const Config = require("../../config.json")
 const Server = require("../Server")
 
 exports.run = (Service, Data, args) => {
-  if(!Config.allowLurking) return Service.io.sockets.connected[Service.session.socketID].emit("methodResult", {
+  if (!Config.allowLurking) return Service.io.sockets.connected[Service.session.socketID].emit("methodResult", {
     success: false,
     method: "lurkAttempt",
     type: "dissallowedByServer",
     message: `This server does not allow lurking.`
   })
-   Service.session.lurking = !Service.session.lurking
-   let memberList
-    try {
-      memberList = Server.getMemberList(Service.sessions, Service.User)
-    } catch (e) {
-      return
-    }
-    Service.io.sockets.in(Service.session.channel).emit("methodResult", {
+  Service.session.lurking = !Service.session.lurking
+  let memberList
+  try {
+    memberList = Server.getMemberList(Service.sessions, Service.User, Service.session.channel)
+  } catch (e) {
+    return
+  }
+  Service.io.sockets.in(Service.session.channel).emit("methodResult", {
+    success: true,
+    method: "getMemberList",
+    type: "success",
+    message: "Successfully got member list",
+    memberList
+  })
+  let bots = sessions.filter(t => t.bot)
+  memberList = Server.getMemberList(Service.sessions, Service.User)
+  for (let i = 0; i < bots.length; i++) {
+    io.sockets.connected[bots[i].socketID].emit('methodResult', {
       success: true,
       method: "getMemberList",
       type: "success",
       message: "Successfully got member list",
       memberList
     })
+  }
 }
 exports.data = {
   name: "lurk",
